8a2e34791a652293f50f3076d240390d7a2c61ca
João Victor Maieru Teixeira <soljoaomaieru@hotmail.com>
2023-09-12 22:15:07 -0300

    Adição de escalonador PRIOP

--- Simulador/Simulador/Program.cs
+++ Simulador/Simulador/Program.cs
@@ -10,16 +10,19 @@ namespace Simulador
             var escalonadorSJF = new ShortestJobFirst(new ReadyQueue(GeradorProcessos.GeraProcessosPadroes()));
             var escalonadorSRTF = new ShortestRemainingTimeFirst(new ReadyQueue(GeradorProcessos.GeraProcessosPadroes()));
             var escalonadorPRIOC = new Prioc(new ReadyQueue(GeradorProcessos.GeraProcessosPadroes()));
+            var escalonadorPRIOP = new Priop(new ReadyQueue(GeradorProcessos.GeraProcessosPadroes()));
 
             var processosFCFS = escalonadorFCFS.Executa();
             var processosSJF = escalonadorSJF.Executa();
             var processosSRTF = escalonadorSRTF.Executa();
             var processosPRIOC = escalonadorPRIOC.Executa();
+            var processosPRIOP = escalonadorPRIOP.Executa();
 
             Console.WriteLine($"FCFS ---- Tempo Execucao: {CalculoNegocio.TempoMedioExecucao(processosFCFS)} | Tempo de espera: {CalculoNegocio.TempoMedioEspera(processosFCFS)}");
             Console.WriteLine($"SJF ---- Tempo Execucao: {CalculoNegocio.TempoMedioExecucao(processosSJF)} | Tempo de espera: {CalculoNegocio.TempoMedioEspera(processosSJF)}");
             Console.WriteLine($"SRTF ---- Tempo Execucao: {CalculoNegocio.TempoMedioExecucao(processosSRTF)} | Tempo de espera: {CalculoNegocio.TempoMedioEspera(processosSRTF)}");
-            Console.WriteLine($"SRTF ---- Tempo Execucao: {CalculoNegocio.TempoMedioExecucao(processosPRIOC)} | Tempo de espera: {CalculoNegocio.TempoMedioEspera(processosPRIOC)}");
+            Console.WriteLine($"PRIOC ---- Tempo Execucao: {CalculoNegocio.TempoMedioExecucao(processosPRIOC)} | Tempo de espera: {CalculoNegocio.TempoMedioEspera(processosPRIOC)}");
+            Console.WriteLine($"PRIOP ---- Tempo Execucao: {CalculoNegocio.TempoMedioExecucao(processosPRIOP)} | Tempo de espera: {CalculoNegocio.TempoMedioEspera(processosPRIOP)}");
 
             Console.ReadLine();
         }
